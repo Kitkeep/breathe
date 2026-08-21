ca37f874fb2536c3fa2c587ee1628c35f2b0a5dc
Kitkeep <Socialmediamy65@gmail.com>
2026-08-21 13:04:11 +0300

    Update app.py

--- app.py
+++ app.py
@@ -58,7 +58,7 @@ DEFAULT_FORWARD_URLS = [
     "https://kerryconnect360.onrender.com",
     "https://jevicarn-school.onrender.com",
     "https://riverhomes.onrender.com",
-    "https://joyschool.onrender.com/",
+    "https://prime-1-rd0g.onrender.com",
      "https://toror-technology.onrender.com"
 ]
 
